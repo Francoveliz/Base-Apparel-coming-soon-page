2467ef13d2bc45b9ff159a1e6a067052ebce4079
Francoveliz <francoleandroveliz@gmail.com>
2020-06-27 21:03:26 -0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+function formAlert(textbox) {
+
+  if (textbox.value === '') {
+    document.getElementById("alert").style.visibility = "visible";
+    document.getElementById("alert-text").style.visibility = "visible";
+    textbox.setCustomValidity('');
+  } else if (textbox.validity.typeMismatch) {
+    document.getElementById("alert").style.visibility = "visible";
+    document.getElementById("alert-text").style.visibility = "visible";
+      textbox.setCustomValidity('');
+  } else {
+    textbox.setCustomValidity('');
+  }
+
+  return true;
+}
+
+
+
+//
